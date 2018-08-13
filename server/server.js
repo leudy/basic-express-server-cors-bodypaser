15e6ebe490d90af1ebc4bbc321e22976d5d37529
Leudi   Rosario <lrosario@rd.magycorp.com>
2018-08-13 01:24:06 -0400

    enabling moongose provider

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
 // http-server
 var bodyParser = require('body-parser')
@@ -8,7 +9,7 @@ var cors = require('cors')
 //setting configs
 app.use(cors())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 app.use(bodyParser.urlencoded({
     extended: false
 }))
@@ -16,6 +17,15 @@ app.use(bodyParser.json())
 
 //getting controllers files
 var ltController = require('./controllers/lt')
+
+mongoose.connect('mongodb://localhost:27017/ltsdb', {
+    useNewUrlParser: true
+}, (err, res) => {
+    if (err) {
+        throw new Error(err);
+    }
+    console.log('mongose is running')
+})
 
 
 //set controllers reponses
